e8ec36e39c81792bbb56ba3e06eb645917b91566
Peter Miklos <pt.miklos@gmail.com>
2017-02-18 13:43:46 +0000

    error messages added

--- lib/van.rb
+++ lib/van.rb
@@ -8,11 +8,19 @@ class Van
   end
 
   def load_bike(bike)
+    fail "Bike cannot be loaded, van is full" if full?
     @bikes << bike
   end
 
   def unload_bike(bike)
+    fail "The bike is not in the van" unless @bikes.include?(bike)
     @bikes.pop
+  end
+
+  private
+
+  def full?
+    @bikes.size == @capacity
   end
 
 end
